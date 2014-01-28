5139c94d720039843487ba35a1b93567e132b7c6
Anthony Echeverry <C15Anthony.Echeverry@usafa.edu>
2014-01-28 15:57:43 -0700

    commented out 3 bits, added in 8 bits

--- Lab1_Echeverry_Anthony.vhd
+++ Lab1_Echeverry_Anthony.vhd
@@ -17,8 +17,60 @@
 -- Additional Comments: 
 --
 ----------------------------------------------------------------------------------
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+
+
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+--entity Lab1_Echeverry_Anthony is
+--    Port ( A : in  STD_LOGIC;
+--           B : in  STD_LOGIC;
+--           C : in  STD_LOGIC;
+--           X : out  STD_LOGIC;
+--           Y : out  STD_LOGIC;
+--           Z : out  STD_LOGIC);
+--end Lab1_Echeverry_Anthony;
+--
+--architecture Behavioral of Lab1_Echeverry_Anthony is
+--
+--signal A_NOT, B_NOT, C_NOT, D, E, F, G, H, I, J, K: STD_LOGIC;
+--
+--begin
+--
+--
+--
+--
+--
+--A_NOT<=not A;
+--B_NOT<=not B;
+--C_NOT<=not C;
+--D<=B OR C;
+--E<=NOT A AND D;
+--F<=A AND NOT B;
+--G<=NOT B AND C;
+--H<=B AND NOT C;
+--I<=F AND NOT C;
+--X<=E OR I;
+--Y<=G OR H;
+--Z<=C;
+--
+--
+--
+--end Behavioral;
+--
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -29,35 +81,15 @@ use IEEE.STD_LOGIC_1164.ALL;
 --library UNISIM;
 --use UNISIM.VComponents.all;
 
-entity Lab1_Echeverry_Anthony is
-    Port ( A : in  STD_LOGIC;
-           B : in  STD_LOGIC;
-           C : in  STD_LOGIC;
-           X : out  STD_LOGIC;
-           Y : out  STD_LOGIC;
-           Z : out  STD_LOGIC);
-end Lab1_Echeverry_Anthony;
+entity twoscomp is
+port( din : in std_logic_vector(7 downto 0);
+dout : out std_logic_vector(7 downto 0));
+end twoscomp;
 
-architecture Behavioral of Lab1_Echeverry_Anthony is
-
-signal A_NOT, B_NOT, C_NOT, D, E, F, G, H, I, J, K: STD_LOGIC;
+architecture Behavioral of twoscomp is
 
 begin
 
-A_NOT<=not A;
-B_NOT<=not B;
-C_NOT<=not C;
-D<=B OR C;
-E<=NOT A AND D;
-F<=A AND NOT B;
-G<=NOT B AND C;
-H<=B AND NOT C;
-I<=F AND NOT C;
-X<=E OR I;
-Y<=G OR H;
-Z<=C;
-
-
-
-end Behavioral;
-
+--dout <= not(din) + "00000001"; this is wrong, no sign
+dout <= std_logic_vector (unsigned(not din) + 1);
+end Behavioral; 
